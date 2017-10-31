7efbaf4eb6ac13d18fd60ec7b5bef811bd2e634d
branislavantic87 <branislav.antic@cadev.rs>
2017-10-31 16:17:03 +0100

    dodao sam jos 2 fajla

--- populate.js
+++ populate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Movie = require('./models/movie');
 const mongoose = require ('mongoose');
 
@@ -12,22 +11,4 @@ var jedan = {
 
 Movie.create(jedan, (err, created) => {
     if (err) throw err;
-=======
-// populatejs
-const express = require('express');
-const Movie = require('./models/movie');
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/movies_app');
-
-var prvi = {
-    title: 'Super film',
-    genre: 'Action',
-    duration: 90,
-};
-
-Movie.create(prvi, (err, createdPrvi) => {
-    if (err) throw err;
-
->>>>>>> 008d492ef032e3c3b5382d082108cfd5c0dff8ba
 });
--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-=======
-// ROUTES/MOVIES.JS
->>>>>>> 008d492ef032e3c3b5382d082108cfd5c0dff8ba
 const express = require('express');
 const router = express.Router();
 const Movie = require('../models/movie');
 
-<<<<<<< HEAD
 
 //index ruta
 router.get('/movies',(req, res) => {
@@ -28,27 +23,5 @@ router.get('/movies/new', (req, res) => {
 //Update
 
 //Delete
-=======
-// INDEX
-router.get('/movies', (req, res) => {
-    Movie.find({}, (err, allMovies) => {
-        if (err) throw err;
-        res.json(allMovies);
-    });
-});
-// NEW 
-router.get('/movies/new', (req, res) => {
-    res.render('movies/new');
-});
-// CREATE
-
-// SHOW 
-
-// EDIT 
-
-// UPDATE
-
-// DESTROY
->>>>>>> 008d492ef032e3c3b5382d082108cfd5c0dff8ba
 
 module.exports = router;
