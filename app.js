427b27cbb92f42c87d51580d15633880cb1fa940
branislavantic87 <branislav.antic@cadev.rs>
2017-11-06 14:57:26 +0100

    dodao sam jos ruta

--- app.js
+++ app.js
@@ -28,3 +28,4 @@ app.get('*', (req, res) => {
 app.listen(8000, () => {
     console.log("Server is running");
 });  
+ 
--- routes/movies.js
+++ routes/movies.js
@@ -15,13 +15,57 @@ router.get('/movies/new', (req, res) => {
     res.render('movies/new');
 });
 //Create
-
+router.post('/movies', (req, res) => {
+    var newMovie =  {
+        title : req.body.title,
+        poster : req.body.poster,
+        genre : req.body.genre,
+        duration : req.body.duration,
+        releaseDate : Date.parse(req.body.releaseDate)
+    }
+    console.log(req.body);
+    if(!newMovie.poster) delete newMovie.poster;
+    Movie.create(newMovie, (err, createdMovie) => {
+        if (err) {
+            res.redirect('/movies')
+        } else {
+        res.json(newMovie);
+        }
+    });
+});
 //Show 
-
+router.get('/movies/:id', (req, res) => {
+    var id = req.params.id;
+    Movie.findById(id, (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
 //Edit
-
+router.get('/movies/:id/edit', (req, res) => {
+    var id = req.params.id;
+    Movie.findById(id, (err, result) => {
+        if (err) throw err;
+        res.render('movies/edit', {result});
+    });
+});
 //Update
-
+router.put('/movies/:id', (req, res) => {
+    var updateMovie =  {
+        $set:{
+        title : req.body.title,
+        poster : req.body.poster,
+        genre : req.body.genre,
+        duration : req.body.duration,
+        releaseDate : req.body.releaseDate
+        }
+    }
+    Movie.findByIdAndUpdate(req.params.id, updateMovie, (err, result) => {
+        if (err) throw err;
+        res.redirect('/movies');
+    })
+});
 //Delete
+
 
 module.exports = router;
